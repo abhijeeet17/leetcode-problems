539d4adb521cbe5584d26ff75c0be1c7fd4a93e1
ABHIJEET SHARMA <151569899+abhijeeet17@users.noreply.github.com>
2026-03-17 22:08:12 +0530

    Time: 92 ms (28.32%), Space: 78.1 MB (30.35%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& arr, int k) {
-        priority_queue<pair<int, vector<int>>> pq;
+        priority_queue<pair<int,vector<int>>>pq;
         for(auto &p : arr){
             int x = p[0];
             int y = p[1];
-            int dist = x*x + y*y;
+            int dist = x * x + y*y;
             pq.push({dist,p});
             if(pq.size()>k) pq.pop();
         }
