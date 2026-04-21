5caf298ebd3f464838553bca6139e66643d82aac
ABHIJEET SHARMA <151569899+abhijeeet17@users.noreply.github.com>
2026-04-21 18:28:51 +0530

    Time: 3 ms (27.98%) | Memory: 17.1 MB (71.69%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -1,31 +1,20 @@
-
 class Solution {
 public:
-    int level(TreeNode*root){
-        if(root==NULL) return 0; 
-        return 1+ max(level(root->left), level(root->right));
+    void dfs(TreeNode* root, int depth, vector<vector<int>>& ans) {
+        if (!root) return;
+
+        if (depth == ans.size())
+            ans.push_back(vector<int>());
+
+        ans[depth].push_back(root->val);
+
+        dfs(root->left, depth + 1, ans);
+        dfs(root->right, depth + 1, ans);
     }
-    void nlevel(TreeNode*root, int level,int curr,vector<int>&v){
-        if(root==NULL) return;
-        if(curr==level){
-            v.push_back(root->val);
-            return;
-        }
-        nlevel(root->left,level,curr+1,v);
-        nlevel(root->right,level, curr+1, v);
-    }
-    void nlevelprint(TreeNode*root,vector<vector<int>>&ans){
-         int n = level(root);
-         for(int i = 0; i<n; i++){
-            vector<int>v;
-            nlevel(root,i,0,v);
-            ans.push_back(v);
-            cout<<endl;
-         }
-    } 
+
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>>ans;
-        nlevelprint(root,ans);
+        vector<vector<int>> ans;
+        dfs(root, 0, ans);
         return ans;
     }
 };
