0d4571a27bbe4bf8ca2ecb0937532e58fc0bb50c
ABHIJEET SHARMA <151569899+abhijeeet17@users.noreply.github.com>
2026-04-21 23:20:58 +0530

    Time: 159 ms (51.31%) | Memory: 153.1 MB (66.49%) - LeetSync

---
+++ 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.cpp
@@ -0,0 +1,59 @@
+class DSU {
+public:
+    vector<int> parent;
+
+    DSU(int n) {
+        parent.resize(n);
+        for(int i = 0; i < n; i++) parent[i] = i;
+    }
+
+    int find(int x) {
+        if(parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    void unite(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+        if(pa != pb) parent[pa] = pb;
+    }
+};
+
+class Solution {
+public:
+    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
+        int n = source.size();
+
+        DSU dsu(n);
+
+        for(auto &e : allowedSwaps) {
+            dsu.unite(e[0], e[1]);
+        }
+
+        unordered_map<int, vector<int>> groups;
+        for(int i = 0; i < n; i++) {
+            groups[dsu.find(i)].push_back(i);
+        }
+
+        int ans = 0;
+
+        for(auto &g : groups) {
+            unordered_map<int, int> freq;
+
+            for(int idx : g.second) {
+                freq[source[idx]]++;
+            }
+
+            for(int idx : g.second) {
+                if(freq[target[idx]] > 0) {
+                    freq[target[idx]]--;
+                } else {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
