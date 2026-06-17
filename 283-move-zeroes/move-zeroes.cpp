e94dac1aac445dbc756e3f9b80b3017ffcf82636
ABHIJEET SHARMA <151569899+abhijeeet17@users.noreply.github.com>
2026-06-17 18:06:43 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (82.35%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& arr) {
-        vector<int>temp;
-        int n = arr.size();
-        for(int i = 0; i< n; i++){
-            if(arr[i] != 0){
-                temp.push_back(arr[i]);
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
             }
         }
-        for(int i = 0; i<temp.size();i++){
-            arr[i] = temp[i];
+        while(j<n){
+            nums[j] = 0;
+            j++;
         }
-
-        for(int i = temp.size(); i<n; i++){
-            arr[i] = 0;
-        }
+        
     }
 };
