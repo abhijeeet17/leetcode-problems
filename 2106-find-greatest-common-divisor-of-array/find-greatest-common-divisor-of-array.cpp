865cf1732ca38887d4b982fe8a0b579ccbef7fc7
ABHIJEET SHARMA <151569899+abhijeeet17@users.noreply.github.com>
2026-06-19 19:48:33 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (92.87%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
@@ -4,13 +4,11 @@ public:
         int n = nums.size();
         int a = nums[0];
         int b = nums[0];
-        for (int i = 0; i < n; i++) {
-            if (a > nums[i])
-                a = nums[i];
+        for(int i = 0; i< n; i++){
+            if(a > nums[i]) a = nums[i];
             if(b < nums[i]) b = nums[i];
         }
-        while( b!=0)
-        {
+        while(b != 0){
             int temp = b;
             b = a % b;
             a = temp;
