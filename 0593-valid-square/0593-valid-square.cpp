9bcc2c5f310905113080ae063ac1912a83cbb5be
ABHIJEET SHARMA <151569899+abhijeeet17@users.noreply.github.com>
2026-09-22 23:11:29 +0530

    Time: 0 ms (100%), Space: 29.5 MB (90.82%) - LeetHub

---
+++ 0593-valid-square/0593-valid-square.cpp
@@ -0,0 +1,12 @@
+class Solution{
+public:
+    int dist(vector<int>&a,vector<int>&b){
+        int x=a[0]-b[0],y=a[1]-b[1];
+        return x*x+y*y;
+    }
+    bool validSquare(vector<int>&p1,vector<int>&p2,vector<int>&p3,vector<int>&p4){
+        vector<int>d={dist(p1,p2),dist(p1,p3),dist(p1,p4),dist(p2,p3),dist(p2,p4),dist(p3,p4)};
+        sort(d.begin(),d.end());
+        return d[0]>0&&d[0]==d[1]&&d[1]==d[2]&&d[2]==d[3]&&d[4]==d[5];
+    }
+};
